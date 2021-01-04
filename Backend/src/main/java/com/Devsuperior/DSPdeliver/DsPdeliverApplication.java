41d56719ac327c5ee813488f630d6c056c946b3e
pediasprogramer <pedrodiasdesenvolvedor123@gmail.com>
2021-01-04 11:48:05 -0300

    Project created

---
+++ Backend/src/main/java/com/Devsuperior/DSPdeliver/DsPdeliverApplication.java
@@ -0,0 +1,13 @@
+package com.Devsuperior.DSPdeliver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsPdeliverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsPdeliverApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Devsuperior/DSPdeliver/DsPdeliverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Devsuperior.DSPdeliver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsPdeliverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
